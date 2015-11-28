c173e1285f3f2e9588dee69a041946d7f8b3a96a
Ján Porubenský <jan.porubensky@student.tuke.sk>
2015-11-28 18:38:30 +0100

    String Entity send, Json accept, parse to string

--- Hackathon/app/src/main/java/codebusters/hackathon/hackathon/FileUpload.java
+++ Hackathon/app/src/main/java/codebusters/hackathon/hackathon/FileUpload.java
@@ -2,18 +2,16 @@ package codebusters.hackathon.hackathon;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.ByteArrayBody;
-import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
@@ -22,45 +20,64 @@ import java.io.InputStreamReader;
 
 public class FileUpload extends Activity {
     Bitmap bm;
+    private byte[] byteImage_photo;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        try {
+       // try {
             // bm = BitmapFactory.decodeResource(getResources(),
             // R.drawable.forest);
-            bm = BitmapFactory.decodeFile("/sdcard/DCIM/forest.png");
-            executeMultipartPost();
-        } catch (Exception e) {
-            Log.e(e.getClass().getName(), e.getMessage());
-        }
+        //    bm = BitmapFactory.decodeFile("/sdcard/DCIM/forest.png");
+        //    executeMultipartPost();
+      //  } catch (Exception e) {
+      //      Log.e(e.getClass().getName(), e.getMessage());
+      //  }
     }
 
     @SuppressWarnings("deprecation")
-    public void executeMultipartPost() throws Exception {
+    public void executeMultipartPost(String source) throws Exception {
         try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            bm.compress(CompressFormat.JPEG, 75, bos);
-            byte[] data = bos.toByteArray();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            options.inSampleSize = 4;
+            options.inPurgeable = true;
+            Bitmap bm = BitmapFactory.decodeFile(source,options);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            bm.compress(Bitmap.CompressFormat.JPEG,40,baos);
+
+
+            // bitmap object
+
+            byteImage_photo = baos.toByteArray();
+
+            //generate base64 string of image
+
+            String encodedImage = Base64.encodeToString(byteImage_photo, Base64.DEFAULT);
+
+            //send this encoded string to server
+           // bm = BitmapFactory.decodeFile(source);
+          //  ByteArrayOutputStream bos = new ByteArrayOutputStream();
+          //  bm.compress(CompressFormat.JPEG, 75, bos);
+           // byte[] data = bos.toByteArray();
             HttpClient httpClient = new DefaultHttpClient();
             HttpPost postRequest = new HttpPost(
                     "http://10.0.2.2/cfc/iphoneWebservice.cfc?returnformat=json&amp;method=testUpload");
-            ByteArrayBody bab = new ByteArrayBody(data, "forest.jpg");
-            // File file= new File("/mnt/sdcard/forest.png");
-            // FileBody bin = new FileBody(file);
-            MultipartEntity reqEntity = new MultipartEntity(
-                    HttpMultipartMode.BROWSER_COMPATIBLE);
-            reqEntity.addPart("uploaded", bab);
-            reqEntity.addPart("photoCaption", new StringBody("sfsdfsdf"));
-            postRequest.setEntity(reqEntity);
+           // ByteArrayBody bab = new ByteArrayBody(data, name);
+         //   MultipartEntity reqEntity = new MultipartEntity(
+               //     HttpMultipartMode.BROWSER_COMPATIBLE);
+            postRequest.setEntity(new StringEntity(encodedImage));
+            HttpResponse resp = httpClient.execute(postRequest);
+            HttpEntity ent = resp.getEntity();
             HttpResponse response = httpClient.execute(postRequest);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    response.getEntity().getContent(), "UTF-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
             String sResponse;
             StringBuilder s = new StringBuilder();
-
+          //  System.out.println(reader);
             while ((sResponse = reader.readLine()) != null) {
                 s = s.append(sResponse);
             }
@@ -68,6 +85,7 @@ public class FileUpload extends Activity {
         } catch (Exception e) {
             // handle exception here
             Log.e(e.getClass().getName(), e.getMessage());
+            bm.recycle();
         }
     }
 }
