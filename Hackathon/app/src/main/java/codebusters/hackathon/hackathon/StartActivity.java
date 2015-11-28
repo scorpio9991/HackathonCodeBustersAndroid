fa7413f19d234fb9d5be7a3ff46e260fe3c9c9b0
McDominick <pituch.dominik@gmail.com>
2015-11-28 16:24:05 +0100

    add activities

--- Hackathon/app/src/main/java/codebusters/hackathon/hackathon/StartActivity.java
+++ Hackathon/app/src/main/java/codebusters/hackathon/hackathon/StartActivity.java
@@ -1,12 +1,11 @@
 package codebusters.hackathon.hackathon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +13,9 @@ public class StartActivity extends ActionBarActivity {
         setContentView(R.layout.activity_start);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void addClicked(View view) {
+        Intent event = new Intent(this, EventActivity.class);
+        startActivity(event);
+        finish();
     }
 }
