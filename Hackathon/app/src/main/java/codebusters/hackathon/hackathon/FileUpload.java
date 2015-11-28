b1d67b8a6d56b963f1df7218765679a23242aa4e
Ján Porubenský <jan.porubensky@student.tuke.sk>
2015-11-28 17:05:10 +0100

    MySQL connect+seding, Https first tries

---
+++ Hackathon/app/src/main/java/codebusters/hackathon/hackathon/FileUpload.java
@@ -0,0 +1,73 @@
+package codebusters.hackathon.hackathon;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+
+public class FileUpload extends Activity {
+    Bitmap bm;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        try {
+            // bm = BitmapFactory.decodeResource(getResources(),
+            // R.drawable.forest);
+            bm = BitmapFactory.decodeFile("/sdcard/DCIM/forest.png");
+            executeMultipartPost();
+        } catch (Exception e) {
+            Log.e(e.getClass().getName(), e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public void executeMultipartPost() throws Exception {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bm.compress(CompressFormat.JPEG, 75, bos);
+            byte[] data = bos.toByteArray();
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(
+                    "http://10.0.2.2/cfc/iphoneWebservice.cfc?returnformat=json&amp;method=testUpload");
+            ByteArrayBody bab = new ByteArrayBody(data, "forest.jpg");
+            // File file= new File("/mnt/sdcard/forest.png");
+            // FileBody bin = new FileBody(file);
+            MultipartEntity reqEntity = new MultipartEntity(
+                    HttpMultipartMode.BROWSER_COMPATIBLE);
+            reqEntity.addPart("uploaded", bab);
+            reqEntity.addPart("photoCaption", new StringBody("sfsdfsdf"));
+            postRequest.setEntity(reqEntity);
+            HttpResponse response = httpClient.execute(postRequest);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent(), "UTF-8"));
+            String sResponse;
+            StringBuilder s = new StringBuilder();
+
+            while ((sResponse = reader.readLine()) != null) {
+                s = s.append(sResponse);
+            }
+            System.out.println("Response: " + s);
+        } catch (Exception e) {
+            // handle exception here
+            Log.e(e.getClass().getName(), e.getMessage());
+        }
+    }
+}
--- src/hackethonandriod/MySQLAccess.java
+++ src/hackethonandriod/MySQLAccess.java
@@ -12,118 +12,65 @@ package hackethonandriod;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MySQLAccess {
-  private Connection connect = null;
-  private Statement statement = null;
-  private PreparedStatement preparedStatement = null;
-  private ResultSet resultSet = null;
 
-  public void readDataBase() throws Exception {
-    try {
-      // This will load the MySQL driver, each DB has its own driver
-      Class.forName("com.mysql.jdbc.Driver");
-      // Setup the connection with the DB
-      connect = DriverManager
-          .getConnection("jdbc:mysql://localhost/feedback?"
-              + "user=sqluser&password=sqluserpw");
+    private PreparedStatement pst;
+    private Connection con;
+    private String author;
 
-      // Statements allow to issue SQL queries to the database
-      statement = connect.createStatement();
-      // Result set get the result of the SQL query
-      resultSet = statement
-          .executeQuery("select * from feedback.comments");
-      writeResultSet(resultSet);
+    public void readDataBase() {
+        con = null;
 
-      // PreparedStatements can use variables and are more efficient
-      preparedStatement = connect
-          .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
-      // "myuser, webpage, datum, summery, COMMENTS from feedback.comments");
-      // Parameters start with 1
-      preparedStatement.setString(1, "Test");
-      preparedStatement.setString(2, "TestEmail");
-      preparedStatement.setString(3, "TestWebpage");
-      preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-      preparedStatement.setString(5, "TestSummary");
-      preparedStatement.setString(6, "TestComment");
-      preparedStatement.executeUpdate();
+        String url = "bossqone.eu";
+        String user = "hackathon";
+        String password = "hackathon";
 
-      preparedStatement = connect
-          .prepareStatement("SELECT myuser, webpage, datum, summery, COMMENTS from feedback.comments");
-      resultSet = preparedStatement.executeQuery();
-      writeResultSet(resultSet);
+        try {
 
-      // Remove again the insert comment
-      preparedStatement = connect
-      .prepareStatement("delete from feedback.comments where myuser= ? ; ");
-      preparedStatement.setString(1, "Test");
-      preparedStatement.executeUpdate();
-      
-      resultSet = statement
-      .executeQuery("select * from feedback.comments");
-      writeMetaData(resultSet);
-      
-    } catch (Exception e) {
-      throw e;
-    } finally {
-      close();
+            con = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MySQLAccess.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        }
     }
 
-  }
+    public void write_into_database(String name, String Description, String Location, int id) throws SQLException {
+        if (this.author == null) {
+            System.err.println("Input author?"); //TODO: Better author input
+        }
+        pst = null;
+        pst = con.prepareStatement("INSERT INTO DB VALUES(?,?,?,?)");
+        pst.setString(1, name);
+        pst.setString(2, Description);
+        pst.setString(3, Location);
+        pst.setInt(4, id);
+        pst.executeUpdate();
+        try {
+            if (pst != null) {
+                pst.close();
+            }
+            if (con != null) {
+                con.close();
+            }
 
-  private void writeMetaData(ResultSet resultSet) throws SQLException {
-    //   Now get some metadata from the database
-    // Result set get the result of the SQL query
-    
-    System.out.println("The columns in the table are: ");
-    
-    System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-    for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-      System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MySQLAccess.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
-  }
 
-  private void writeResultSet(ResultSet resultSet) throws SQLException {
-    // ResultSet is initially before the first data set
-    while (resultSet.next()) {
-      // It is possible to get the columns via name
-      // also possible to get the columns via the column number
-      // which starts at 1
-      // e.g. resultSet.getSTring(2);
-      String user = resultSet.getString("myuser");
-      String website = resultSet.getString("webpage");
-      String summery = resultSet.getString("summery");
-      Date date = resultSet.getDate("datum");
-      String comment = resultSet.getString("comments");
-      System.out.println("User: " + user);
-      System.out.println("Website: " + website);
-      System.out.println("Summery: " + summery);
-      System.out.println("Date: " + date);
-      System.out.println("Comment: " + comment);
+    public void set_Author(String author) {
+        this.author = author;
+
     }
-  }
+}
+
+
 
   // You need to close the resultSet
-  private void close() {
-    try {
-      if (resultSet != null) {
-        resultSet.close();
-      }
-
-      if (statement != null) {
-        statement.close();
-      }
-
-      if (connect != null) {
-        connect.close();
-      }
-    } catch (Exception e) {
-
-    }
-  }
-
-} 
